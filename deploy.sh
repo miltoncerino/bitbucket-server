b928d599a4eee0d5e00370d50f828525fcbbfbf8
Milton Cerino <miltoncerino@gmail.com>
2020-01-21 16:36:03 -0300

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,11 @@
 source tools/colors.cfg
 source config/environment.cfg
 
-function CreateBackupDirectory {
-  if [ -d $1 ]; then
-    echo "Backup directory exists"
-  else
-    sudo mkdir $1 && \
-    echo "Backup directory created"
-  fi
-}
-
 function CreateVolume {
   if docker volume inspect $1 > /dev/null 2>&1; then
-    echo "Jenkins volume is already created"
+    echo "Bitbucket volume $1 is already present"
   else
-    echo "Creating Jenkins data volume"
+    echo "Creating Bitbucket volume $1"
     docker volume create $1
     if [ $? -eq 0 ]; then
       echo "DONE"
@@ -27,43 +18,7 @@ function CreateVolume {
 
 echo -e ${COLOR_CYAN}
 
-#CreateBackupDirectory ${JENKINS_BACKUP_DIRECTORY}
-#CreateVolume ${JENKINS_VOLUME}
+CreateVolume ${BITBUCKET_DB_VOLUME_NAME}
+CreateVolume ${BITBUCKET_VOLUME_NAME}
 
-echo "Building Postgres image..."
-docker run \
-  --rm
-  -v bitbucketDbVolume:/var/lib/postgresql/data \
-  --name bitbucketDB \
-  -e POSTGRES_PASSWORD=postgres \
-  -e POSTGRES_USER=postgres \
-  -e POSTGRES_DB=postgres \
-  -e PGDATA=/var/lib/postgresql/data/pgdata \
-  -p 5432:5432 \
-  -d \
-  postgres:9.6
-
-
-
-
-docker build -t ${JENKINS_IMAGE_NAME} .
-echo "Creating Jenkins container..."
-docker run \
-  --rm \
-  -itd \
-  --name ${JENKINS_CONTAINER_NAME} \
-  -p 8080:8080 \
-  -p 50000:50000 \
-  -v ${JENKINS_VOLUME}:/var/jenkins_home \
-  -v ${JENKINS_BACKUP_DIRECTORY}:/var/backups \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -u root \
-  ${JENKINS_IMAGE_NAME}
-echo "DONE"
-
-echo ""
-echo "* You can inspect the recently created volume by running:"
-echo "    docker volume inspect ${JENKINS_VOLUME}"
-echo ""
-echo "* You can find the mount point here:"
-echo "    /var/lib/docker/volumes/${JENKINS_VOLUME}/_data"
+docker-compose -f bitbucket-compose.yaml up -d
